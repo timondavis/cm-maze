c1e22c622d40d51b327782b01400fac2a333f1bc
Timon Davis <timondavis@yahoo.com>
2018-12-11 16:57:15 -0800

    Makes TS work

---
+++ lib/index.d.ts
@@ -0,0 +1,21 @@
+import {Maze} from './Maze';
+import {MazeBuilder} from './MazeBuilder';
+import {MazeNode} from './MazeNode';
+import {Cardinality} from "./Behavior/Cardinality";
+import {Compass4} from "./Behavior/Compass4";
+import {Compass8} from "./Behavior/Compass8"
+import {NodeLocation} from "./MazeCoordinates/NodeLocation";
+import {NodeLocation2D} from "./MazeCoordinates/NodeLocation2D";
+import {MazeRenderer} from "./Render/MazeRenderer";
+
+export{
+    Maze,
+    MazeBuilder,
+    MazeNode,
+    Cardinality,
+    Compass4,
+    Compass8,
+    NodeLocation,
+    NodeLocation2D,
+    MazeRenderer
+};
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,21 @@
-import {MazeBuilder} from "./MazeBuilder";
+import {Maze} from './Maze';
+import {MazeBuilder} from './MazeBuilder';
+import {MazeNode} from './MazeNode';
+import {Cardinality} from "./Behavior/Cardinality";
+import {Compass4} from "./Behavior/Compass4";
+import {Compass8} from "./Behavior/Compass8"
+import {NodeLocation} from "./MazeCoordinates/NodeLocation";
+import {NodeLocation2D} from "./MazeCoordinates/NodeLocation2D";
 import {MazeRenderer} from "./Render/MazeRenderer";
 
-declare var define: any;
-declare var module: any;
-
-( function() {
-
-
-    let exportables = [MazeBuilder, MazeRenderer];
-
-    if ( typeof module !== 'undefined' && typeof module.exports !== 'undefined' ) {
-        exportables.forEach( exp => module.exports[nameof(exp)] = exp );
-    } else if ( typeof define === 'function' && define.amd ) {
-        exportables.forEach( exp => define( () => exp ) );
-    } else if ( window ) {
-        exportables.forEach( exp => (window as any )[nameof(exp)] = exp );
-    }
-
-    function nameof(fn: any): string {
-        return typeof fn === 'undefined' ? '' : fn.name ? fn.name : (() => {
-            let result = /^function\s+([\w\$]+)\s*\(/.exec(fn.toString());
-            return !result ? '' : result[1];
-        })();
-    }
-
-
-})();
-module.exports.MazeBuilder = MazeBuilder;
+export{
+    Maze,
+    MazeBuilder,
+    MazeNode,
+    Cardinality,
+    Compass4,
+    Compass8,
+    NodeLocation,
+    NodeLocation2D,
+    MazeRenderer
+};
